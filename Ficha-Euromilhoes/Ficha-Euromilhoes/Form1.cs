16255e00f7a9cf977fb306762977e4910060ba3e
kallebysantos <60856996+kallebysantos@users.noreply.github.com>
2020-11-24 16:23:11 +0000

    feat: Icone do app

--- Ficha-Euromilhoes/Ficha-Euromilhoes/Form1.cs
+++ Ficha-Euromilhoes/Ficha-Euromilhoes/Form1.cs
@@ -16,5 +16,10 @@ namespace Ficha_Euromilhoes
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
